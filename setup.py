43050dc9874ed01e153c0713ebb63622b79f429e
cloomis <cloomis@localhost>
2009-10-20 12:42:37 +0000

    add basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""Install this package. Requires sdss3tools.
+
+To use:
+python setup.py install
+"""
+import sdss3tools
+
+sdss3tools.setup(
+    description = "Code base for SDSS-III SOP actor",
+)
