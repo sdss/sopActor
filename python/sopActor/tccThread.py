ae98c434c8cc6ca72ea91f2cdf8b5ac26c88c466
cloomis <cloomis@localhost>
2010-05-26 23:05:25 +0000

    add keepOffsets support

--- python/sopActor/tccThread.py
+++ python/sopActor/tccThread.py
@@ -37,6 +37,7 @@ def main(actor, queues):
                 except AttributeError, e:
                     startSlew = True
                     
+                # Do not _start_ slew if an axis is wedged.
                 if tccState.badStat:
                     cmd.warn('text="in slew with badStat=%s halted=%s slewing=%s"' % \
                                  (tccState.badStat, tccState.halted, tccState.slewing))
@@ -52,15 +53,22 @@ def main(actor, queues):
                     import time; time.sleep(1)
                     queues[sopActor.TCC].put(Msg.SLEW, cmd=msg.cmd,
                                              replyQueue=msg.replyQueue, waitForSlewEnd=True)
-
                     continue
+
+                # Yuck, yuck, yuck. At the very least we should limit which offsets are kept.
+                try:
+                    keepArgs = "/keep=(obj,arc,gcorr,calib,bore)" if msg.keepOffsets else ""
+                except:
+                    keepArgs = ""
 
                 try:
                     cmd.inform('text="slewing to (%.04f, %.04f, %g)"' % (msg.ra, msg.dec, msg.rot))
+                    if keepArgs:
+                        cmd.warn('text="keeping all offsets"')
                     
                     cmdVar = msg.actorState.actor.cmdr.call(actor="tcc", forUserCmd=cmd,
-                                                            cmdStr="track %f, %f icrs /rottype=object/rotang=%g/rotwrap=mid" % \
-                                                            (msg.ra, msg.dec, msg.rot))
+                                                            cmdStr="track %f, %f icrs /rottype=object/rotang=%g/rotwrap=mid %s" % \
+                                                            (msg.ra, msg.dec, msg.rot, keepArgs))
                 except AttributeError:
                     cmd.inform('text="slewing to (az, alt, rot) == (%.04f, %.04f, %0.4f)"' % (msg.az, msg.alt, msg.rot))
                     
