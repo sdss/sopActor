186dd55bde6bc1de8429e0c37a1df4b762af3471
cloomis <cloomis@localhost>
2011-01-18 15:45:33 +0000

    start long-running guide loop

--- python/sopActor/guiderThread.py
+++ python/sopActor/guiderThread.py
@@ -46,38 +46,43 @@ def main(actor, queues):
                 forceOpt = "force" if (hasattr(msg, 'force') and msg.force) else ""
                 oneExposureOpt = "oneExposure" if (hasattr(msg, 'oneExposure') and msg.oneExposure) else ""
                 clearCorrections = "clearCorrections" if (hasattr(msg, 'clearCorrections')
-														  and msg.clearCorrections) else ""
+                                                          and msg.clearCorrections) else ""
 
-				if clearCorrections:
-					for corr in ("axes", "scale", "focus"):
-						cmdVar = actorState.actor.cmdr.call(actor="guider", forUserCmd=msg.cmd,
-															cmdStr=("%s off" % (corr)),
-															keyVars=[], timeLim=3)
-						if cmdVar.didFail:
-							cmd.cmd.warn('text="failed to disable %s guider corrections!!!"' (corr))
-							msg.replyQueue.put(Msg.DONE, cmd=msg.cmd, success=not cmdVar.didFail)
-							continue
-					
+                if clearCorrections:
+                    for corr in ("axes", "scale", "focus"):
+                        cmdVar = actorState.actor.cmdr.call(actor="guider", forUserCmd=msg.cmd,
+                                                            cmdStr=("%s off" % (corr)),
+                                                            keyVars=[], timeLim=3)
+                        if cmdVar.didFail:
+                            cmd.cmd.warn('text="failed to disable %s guider corrections!!!"' (corr))
+                            msg.replyQueue.put(Msg.DONE, cmd=msg.cmd, success=not cmdVar.didFail)
+                            continue
+                    
                 timeLim = msg.expTime   # seconds
 
                 # If we are starting a "permanent" guide loop, we can't wait for the command to finish.
                 # But wait long enough to see whether it blows up on the pad. Ugh - CPL.
-                if oneExposureOpt:
-                    timeLim += 100
-                else:
-                    timeLim += 10
+                timeLim += 15
+                cmdStr = "%s %s %s %s" % (("on" if msg.on else "off"),
+                                          expTimeOpt, forceOpt, oneExposureOpt)
                 cmdVar = actorState.actor.cmdr.call(actor="guider", forUserCmd=msg.cmd,
-                                                    cmdStr="%s %s %s %s" % (("on" if msg.on else "off"),
-                                                                       expTimeOpt, forceOpt, oneExposureOpt),
+                                                    cmdStr=cmdStr,
                                                     keyVars=[], timeLim=timeLim)
-				if msg.on and not oneExposureOpt:
-					# Waiting for word on whether there's a proper way to distinguish between
-					# 'failed' and 'times out' -- CPL
-                    if cmdVar.maxEndTime and (cmdVar.maxEndTime < time.time()):
+                if msg.on and not oneExposureOpt:
+                    # Waiting for word on whether there's a proper way to distinguish between
+                    # 'failed' and 'timed out' -- CPL
+                
+                    if "Timeout" in cmdVar.lastReply.keywords:
                         # command timed out -- assume the loop is running OK.
                         msg.replyQueue.put(Msg.DONE, cmd=msg.cmd, success=True)
-                        continue
-                    
+                    else:
+                        cmd.warn('text="probably failed to start guide exposure loop: %s"' % (cmdStr))
+                        msg.replyQueue.put(Msg.DONE, cmd=msg.cmd, success=False)
+                    continue
+                else:
+                    if cmdVar.didFail:
+                        cmd.warn('text="guider command failed: %s"' % (cmdStr))
+                           
                 msg.replyQueue.put(Msg.DONE, cmd=msg.cmd, success=not cmdVar.didFail)
 
             elif msg.type == Msg.EXPOSE:
