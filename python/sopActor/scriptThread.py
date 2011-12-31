52ad5c3a0a490dde979147d418453b664c7ff0b2
cloomis <cloomis@localhost>
2011-12-31 22:26:57 +0000

    have the script thread handle script cmd finish/fail by itself

--- python/sopActor/scriptThread.py
+++ python/sopActor/scriptThread.py
@@ -18,7 +18,7 @@ def main(actor, queues):
     been thought through."""
 
     threadName = "script"
-    myQueueName = SCRIPT
+    myQueueName = sopActor.SCRIPT
     runningScript = None
 
     while True:
@@ -27,8 +27,7 @@ def main(actor, queues):
 
         try:
             msg = actorState.queues[myQueueName].get(timeout=timeout)
-            msg.cmd.warn('text="Msg=%s, sopActor=%s, REPLY=%s"' % (Msg, None, Msg.REPLY))
-            import pdb; pdb.set_trace()
+
             if msg.type == Msg.EXIT:
                 if msg.cmd:
                     msg.cmd.inform("text=\"Exiting thread %s\"" % (threading.current_thread().name))
@@ -36,45 +35,47 @@ def main(actor, queues):
 
             elif msg.type == Msg.NEW_SCRIPT:
                 if runningScript:
-                    msg.cmd.warn('text="%s thread is already running a script: %s"' %
-                             (threadName, runningScript.name))
-                    msg.replyQueue.put(Msg.REPLY, cmd=msg.cmd, success=False)
+                    msg.cmd.fail('text="%s thread is already running a script: %s"' %
+                                 (threadName, runningScript.name))
+                    continue
+
                 scriptName = msg.scriptName
                 runningScript = script.Script(msg.cmd, scriptName)
-                actorState.queues[myQueueName].put(Msg.SCRIPT_STEP, msg.cmd, replyQueue=msg.replyQueue)
+                actorState.queues[myQueueName].put(Msg.SCRIPT_STEP, msg.cmd)
 
             elif msg.type == Msg.SCRIPT_STEP:
                 if not runningScript:
-                    msg.cmd.warn('text="%s thread is not running a script, so cannot step it."' %
+                    msg.cmd.fail('text="%s thread is not running a script, so cannot step it."' %
                              (threadName))
-                    msg.replyQueue.put(Msg.REPLY, cmd=msg.cmd, success=False)
+                    continue
+
                 scriptLine = runningScript.fetchNextStep()
                 if not scriptLine:
-                    msg.cmd.warn('text="script %s appears to be done"' % (runningScript.name))
+                    msg.cmd.finish('text="script %s appears to be done"' % (runningScript.name))
                     runningScript = None
-                    msg.replyQueue.put(Msg.REPLY, cmd=msg.cmd, success=True)
                     continue
 
                 actorName, cmdStr, maxTime = scriptLine
                 if maxTime == 0.0:
                     maxTime = 30.0
 
-                msg.cmd.warn('text="firing off script line: %s %s (%0.1f)"' % (actorName, cmdStr, maxTime))
+                msg.cmd.warn('text="firing off script line: %s %s (maxTime=%0.1f)"' % (actorName, cmdStr, maxTime))
                 cmdVar = actorState.actor.cmdr.call(actor=actorName, forUserCmd=msg.cmd,
                                                     cmdStr=cmdStr,
                                                     timeLim=maxTime+15)
                 if cmdVar.didFail:
-                    msg.cmd.warn('text="Failed to run %s %s (%s)"' % (actorName, cmdStr, Msg.REPLY))
+                    msg.cmd.fail('text="Failed to run %s %s"' % (actorName, cmdStr))
                     runningScript = None
-                    msg.replyQueue.put(Msg.REPLY, cmd=msg.cmd, success=False)
                 else:
-                    actorState.queues[myQueueName].put(Msg.SCRIPT_STEP, msg.cmd, replyQueue=msg.replyQueue)
+                    actorState.queues[myQueueName].put(Msg.SCRIPT_STEP, msg.cmd)
 
             elif msg.type == Msg.STOP_SCRIPT:
                 if not runningScript:
-                    msg.cmd.warn('text="%s thread is not running a script, so cannot stop it."' %
-                             (threadName))
-                    msg.replyQueue.put(Msg.REPLY, cmd=msg.cmd, success=False)
+                    msg.cmd.fail('text="%s thread is not running a script, so cannot stop it."' %
+                                 (threadName))
+                    continue
+
+                # Just signal that we are done.
                 runningScript.stop()
 
             elif msg.type == Msg.STATUS:
