78c799a759e8864a4d580fbded2a5cf1278d48c3
José Sánchez-Gallego <gallegoj@uw.edu>
2016-07-17 04:35:42 -0400

    added sopActor_main.py

---
+++ python/sopActor/sopActor_main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# sopActor_main.py
+#
+# Created by José Sánchez-Gallego on 17 Jul 2016.
+
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from sopActor import Msg, SOPActor
+
+# Start a new actor
+if __name__ == '__main__':
+
+    sop = SOPActor.SopActor.newActor()
+    sop.run(Msg=Msg)
