e03a103c70d82ac2f6c429b6b9b23e6bca6e9539
j.sanchezgallego <j.sanchezgallego@localhost>
2016-07-19 02:44:50 +0000

    init threads using SOP custom Queue

--- python/sopActor/sopActor_main.py
+++ python/sopActor/sopActor_main.py
@@ -10,10 +10,15 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+import sopActor
 from sopActor import Msg, SopActor
 
 # Start a new actor
 if __name__ == '__main__':
 
     sop = SopActor.SopActor.newActor()
-    sop.run(Msg=Msg)
+
+    # sopActor uses its own subclass of Queue.
+    # We send it to actorcore.SDSSActor so that the threads can be initialised
+    # using it.
+    sop.run(Msg=Msg, queueClass=sopActor.Queue)
