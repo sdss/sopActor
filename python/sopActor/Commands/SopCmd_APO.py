0720300de49faa09257c337fe8f0f1471bc98996
José Sánchez-Gallego <gallegoj@uw.edu>
2017-05-31 19:57:47 -0600

    Revert "Revert "Merge branch 'ticket_2483'""
    This reverts commit 21c77da548e379990ab73910bb93d38a04397090.

---
+++ python/sopActor/Commands/SopCmd_APO.py
@@ -0,0 +1,31 @@
+# !usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# Licensed under a 3-clause BSD license.
+#
+# @Author: Brian Cherinka
+# @Date:   2016-06-10 13:00:58
+# @Last modified by:   Brian
+# @Last Modified time: 2016-06-10 13:04:25
+
+from __future__ import print_function, division, absolute_import
+
+import opscore.protocols.keys as keys
+import opscore.protocols.types as types
+from opscore.utility.qstr import qstr
+
+from sopActor.Commands import SopCmd
+
+
+class SopCmd_APO(SopCmd.SopCmd):
+
+    def __init__(self, actor):
+        # initialize from the superclass
+        super(SopCmd_APO, self).__init__(actor)
+
+        # Define some new command keywords
+
+        # Define new commands for APO
+        pass
+
+
