4c40db464200fa331a8fcffeec6ede231a075ebd
José Sánchez-Gallego <gallegoj@uw.edu>
2020-01-13 11:21:43 -0800

    Cleaner syntax

--- python/sopActor/CmdState.py
+++ python/sopActor/CmdState.py
@@ -437,8 +437,7 @@ class GotoFieldCmd(CmdState):
     def abort(self):
 
         # Before we set the stages to aborted, we check if hartmann was running.
-        is_hartmann_set = 'hartmann' in self.stage
-        is_hartmann_running = (is_hartmann_set and self.stages['hartmann'] == 'running')
+        is_hartmann_running = self.stages.get('hartmann', None) == 'running'
 
         self.doSlew = False
         self.doHartmann = False
